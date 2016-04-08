83ff30d0112d473b8936d37f911ade75efa4b180
cosh-jobb <cobbje@dukes.jmu.edu>
2016-04-08 02:08:16 -0400

    changed again

--- client/js/app.js
+++ client/js/app.js
@@ -5,7 +5,7 @@ angular.module('smartclickr', [
 	'lbServices'
 ])
 
-I added this
+I added this again
 
 .config([ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 	// define client routes, i.e., urls
