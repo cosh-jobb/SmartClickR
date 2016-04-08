97d8e78678d60a493ae24314a6424d7d9f233e91
lammanf <lammanf@dukes.jmu.edu>
2016-04-08 00:58:55 -0400

    putting back app.js?

---
+++ client/js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('smartclickr', [
+	'ui.router',
+	'lbServices'
+])
+
+.config([ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	// define client routes, i.e., urls
+	$stateProvider
+	 .state('features', {
+	 	url: '/features',
+	 	templateUrl: 'views/features.html'
+	 })
+	  .state('login', {
+	 	url: '/login',
+	 	templateUrl: 'views/login.html'
+	 })
+	  .state('command-center', {
+	 	url: '/command-center',
+	 	templateUrl: 'views/command-center.html',
+	 	authenticate: true
+	 })
+	  .state('forbidden', {
+	 	url: '/forbidden',
+	 	templateUrl: 'views/forbidden.html'
+	 })
+	  .state('create', {
+	 	url: '/create',
+	 	templateUrl: 'views/create.html'
+	 })
+	  .state('signup', {
+	 	url: '/signup',
+	 	templateUrl: 'views/signup.html'
+	 })
+	  .state('home', {
+	 	url: '/',
+	 	templateUrl: 'views/home.html'
+	 });
+	 $urlRouterProvider.otherwise('/');
+}]);
