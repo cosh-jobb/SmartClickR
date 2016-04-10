0253dda40e4dea17d8301a3286c6e5d40baa4e62
cosh-jobb <cobbje@dukes.jmu.edu>
2016-04-10 14:41:47 -0400

    pollcreate

--- client/js/services/createpollService.js
+++ client/js/services/createpollService.js
@@ -1,16 +1,16 @@
   angular
   .module('smartclickr')
-  .factory('pollservice', ['Random', 'Poll', '$q', '$rootScope', function(Poll, $q,
-      $rootScope, Random) {
-    function creation(name, description) {
-      console.log('the poll creation is about to commence');
+  .factory('pollservice', ['Poll', '$q', '$rootScope', function(Poll, $q,
+      $rootScope) {
+    function creation(Name, Description) {
+      console.log(Poll);
       	return Poll
       	.create
       	({
 		  Poll_id: 0,
 		  Owner_id: 0,
-		  Name: name,
-		  Description: description,
+		  Name: Name,
+		  Description: Description,
 		  Order: 0,
 		  Sessioncode: 'string',
 		  CreateDate: 2016-04-10,
@@ -18,7 +18,7 @@
 		  EndDate: 2016-04-10,
 		  id: 0,
 		  personId: 0
-        })
+                  })
         .$promise
         .then(function(response) {
           console.log('the login response was ', response);
