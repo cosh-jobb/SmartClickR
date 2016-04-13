afcb3a5fe65921b2bc7195223c81ed93a415af1c
lammanf <lammanf@dukes.jmu.edu>
2016-04-13 03:21:23 -0400

    added more stuff

--- client/js/controllers/question.js
+++ client/js/controllers/question.js
@@ -1,29 +1,54 @@
 angular
   .module('smartclickr')
   .controller('question', 
-    ['$scope', 'Poll', '$rootScope', 'global', 'Question', '$q', '$timeout', function($scope,Poll, $rootScope, global, Question, $q, $timeout) {
+    ['$scope', 'Poll', '$rootScope', 'global', 'Question', '$q', '$timeout',
+     function($scope,Poll, $rootScope, global, Question, $q, $timeout) {
 
       $scope.startup = function () {
         $scope.QNo = 1;
         global.setQuestion($scope.QNo);
-        $scope.SC = global.getAPoll().$promise;
-        $timeout(, 2000);
-        Question.find({
-          filter: {
-            where:{
-              Sessionid: $scope.SC, 
-              QuestionOrder: $scope.QNo
-            }
-          }
-        }).$promise
-        .then(function(response) {
-          $scope.array = response;
-          $scope.notarray = $scope.array[0];
-          $scope.stem = $scope.notarray.Stem;
-        });
-      };
+        $scope.SC = global.getAPoll();
+          console.log($scope.SC);
+            Question.find({
+              filter: {
+                where:{
+                  Sessionid: $scope.SC, 
+                  QuestionOrder: global.getAPoll()
+                }
+              }
+            }).$promise
+            .then(function(response) {
+              $scope.array = response;
+              $scope.notarray = $scope.array[0];
+              $scope.stem = $scope.notarray.Stem;
+            });
 
-      $scope.startup()
-
+        $scope.startup()
+      }
     }]
   );
+
+  function getSC(name) {
+  var deferred = $q.defer();
+
+  function() {
+    deferred.notify('About to greet ' + name + '.');
+
+    if (okToGreet(name)) {
+      deferred.resolve('Hello, ' + name + '!');
+    } else {
+      deferred.reject('Greeting ' + name + ' is not allowed.');
+    }
+  }
+
+  return deferred.promise;
+}
+
+var promise = asyncGreet('Robin Hood');
+promise.then(function(greeting) {
+  alert('Success: ' + greeting);
+}, function(reason) {
+  alert('Failed: ' + reason);
+}, function(update) {
+  alert('Got notification: ' + update);
+});
